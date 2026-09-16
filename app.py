2de762943161b144b13616e8efb50874dc31fcf6
Chitesh Jindal <chiteshjindal11@gmail.com>
2026-09-16 22:58:21 +0530

    added yfinance

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import streamlit as st
-
+import yfinance as yf;
 st.title("Market Intel")
 st.write("AI-Powered Indian Stock Intelligence")
 
 stock = st.text_input("Enter stock symbol", "RELIANCE.NS")
 
 if st.button("Analyze"):
-    st.write("Selected stock:", stock)
+    data = yf.download(stock, period="1mo")
+    #1mo means 1 month of data
+    st.write(data)
+
