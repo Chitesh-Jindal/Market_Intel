5dc76da491d27627d03abcb208b8b570d6c58de8
Chitesh Jindal <chiteshjindal11@gmail.com>
2026-09-17 12:16:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,6 +17,9 @@ if st.button("Analyze"):
         #checking values in data set that are missing
         st.write("Missing Values")
         st.write(missing)
+
+        st.subheader("Analysis for " + stock)
+        
         data["Daily_Return"]=data["Close"].pct_change()*100
         average_return = data["Daily_Return"].mean()
         volatility=data["Daily_Return"].std()
