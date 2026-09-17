869b266325124db535a8d25bfe842f1561dea079
Chitesh Jindal <chiteshjindal11@gmail.com>
2026-09-17 11:15:44 +0530

    added percent change

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ if st.button("Analyze"):
     data = yf.download(stock, period="1mo")
     #1mo means 1 month of data
     if data.empty:
+        #if the stock entered is invalid
         st.error("No data found for this stock")
     else:
         st.write(data)
@@ -16,5 +17,10 @@ if st.button("Analyze"):
         #checking values in data set that are missing
         st.write("Missing Values")
         st.write(missing)
+        data["Daily_Return"]=data["Close"].pct_change()*100
+
+        st.write("Daily Return (%)")
+        st.write(data["Daily_Return"])
+        
         st.line_chart(data["Close"])
 
