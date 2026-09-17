362b4424804f223cf1b4a80f018bdf066784fd16
Chitesh Jindal <chiteshjindal11@gmail.com>
2026-09-17 11:55:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,9 +21,11 @@ if st.button("Analyze"):
         average_return = data["Daily_Return"].mean()
         volatility=data["Daily_Return"].std()
         highest_price=data["Close"].max().item()
+        lowest_price=data["Close"].min().item()
 
         st.write("Average Daily Return: ",round(average_return,2),"%")
         st.write("Daily Volatility: ",round(volatility,2),"%")
         st.write("Highest Closing Price: ",round(highest_price,2))
+        st.write("Lowest Closing Price: ",round(lowest_price,2))
         st.line_chart(data["Close"])
 
