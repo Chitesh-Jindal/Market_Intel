001371d8b1197cb4043c3dac3c9a2f97f72f96d6
Chitesh Jindal <chiteshjindal11@gmail.com>
2026-09-17 10:56:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ stock = st.text_input("Enter stock symbol", "RELIANCE.NS")
 if st.button("Analyze"):
     data = yf.download(stock, period="1mo")
     #1mo means 1 month of data
-    st.write(data)
-    st.line_chart(data["Close"])
+    if data.empty:
+        st.error("No data found for this stock")
+    else
+        st.write(data)
+        st.line_chart(data["Close"])
 
